1325bf5a7c6221d21a6b6651cfac5efeb076a93f
Paul Lionel <paulmcsota@gmail.com>
2022-02-18 18:16:33 +0100

    se retira test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -5,14 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-		String message = "test string value";
-		Assert.assertNotNull(message);
 	}
 
 }
